089cad9fffbd02c71d2c6ff7fd733c82777c0762
ivo708 <ivanrojmer@gmail.com>
2018-05-13 00:11:49 +0200

    Add files via upload

---
+++ usefulscripts/hbmenu.js
@@ -0,0 +1,10 @@
+sc.getServices(["lr"], function (lr) {
+    var path = utils.str2ab("@Sdcard:/loader.nsp"); 
+    var tid  = [0x100D, 0x01000000];        /* TID of the Album applet */
+    var storageId = 3;                      /* NAND (location of the Album applet) */
+
+    var msg = sc.ipcMsg(0).data(storageId).sendTo(lr).assertOk(); /* nn::lr::ILocationResolverManager(StorageId storageId) => nn::lr::ILocationResolver */
+    sc.withHandle(msg.movedHandles[0], (h) => {                   /* nn::lr::ILocationResolver::SetProgramNcaPath(u64 TID, const char *path) */
+        msg = sc.ipcMsg(1).data(tid).xDescriptor(path, path.byteLength, 0).sendTo(h).assertOk();
+    });
+});
